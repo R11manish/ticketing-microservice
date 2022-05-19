37ece80aefe0c2abee6de5c3adb4a6b2272a2d85
R11manish <github.jmtss@gmail.com>
2022-05-19 08:45:01 +0530

    test commit for test script

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it('responds with details about the current user', async () => {
     .get('/api/users/currentUser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
